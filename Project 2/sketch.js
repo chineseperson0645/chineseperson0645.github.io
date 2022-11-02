b9b0c256094587f32d53d8b98c2f370503a39d7b
chineseperson0645 <114017596+chineseperson0645@users.noreply.github.com>
2022-11-01 21:05:40 -0600

    Great Foolishness

--- Project 2/sketch.js
+++ Project 2/sketch.js
@@ -1,91 +1,81 @@
-// Game of Life
+// Randomized 2d Grid
 // Schellenberg
-// Oct 27, 2022
+// Oct 26, 2022
 
-const ROWS = 40;
-const COLS = 40;
+const ROWS = 10;
+const COLS = 30;
 let grid;
 let cellWidth;
 let cellHeight;
-let autoPlay = false;
-let gosperGun;
-
-function preload() {
-  gosperGun = loadJSON("gosper.json");
-}
+let playerX = 0;
+let playerY = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   cellWidth = width/COLS;
   cellHeight = height/ROWS;
-  grid = createRandom2dArray(COLS, ROWS);
+  grid = create2dArray(COLS, ROWS);
+  //place player in grid
+  grid[playerY][playerX] = 9;
 }
 
 function draw() {
   background(220);
-  if (autoPlay && frameCount % 3 === 0) {
-    grid = takeTurn(grid);
-  }
   displayGrid(grid);
 }
 
 function keyPressed() {
-  if (key === "e") {
-    grid = create2dArray(COLS, ROWS);
-  }
-  if (key === " ") {
-    grid = takeTurn(grid);
-  }
-  if (key === "a") {
-    autoPlay = !autoPlay;
-  }
-  if (key === "g") {
-    grid = gosperGun;
-  }
-}
+  if (keyCode === RIGHT_ARROW) {
+    if (grid[playerY][playerX+1] === 0) {
+      //reset old location to white
+      grid[playerY][playerX] = 0;
+      
+      //move
+      playerX++;
 
-function takeTurn(grid) {
-  let nextTurn = create2dArray(COLS, ROWS);
-
-  for (let y=0; y<ROWS; y++) {
-    for (let x=0; x<COLS; x++) {
-      let neighbours = 0;
-
-      //look at all cells around this one...
-      for (let i=-1; i<=1; i++) {
-        for (let j=-1; j<=1; j++) {
-          //edge case check
-          if (y+i >= 0 && y+i < ROWS && x+j >= 0 && x+j < COLS) {
-            neighbours += grid[y+i][x+j];
-          }
-        }
-      }
-
-      //don't count self!
-      neighbours -= grid[y][x];
-
-      //apply rules
-      if (grid[y][x] === 1) { //alive
-        if (neighbours === 2 || neighbours === 3) {
-          nextTurn[y][x] = 1;
-        }
-        else {
-          nextTurn[y][x] = 0;
-        }
-      }
-
-      if (grid[y][x] === 0) { //dead
-        if (neighbours === 3) {
-          nextTurn[y][x] = 1;
-        }
-        else {
-          nextTurn[y][x] = 0;
-        }
-      }
+      //set new player location
+      grid[playerY][playerX] = 9;
     }
   }
 
-  return nextTurn;
+  if (keyCode === LEFT_ARROW) {
+    if (grid[playerY][playerX-1] === 0) {
+      //reset old location to white
+      grid[playerY][playerX] = 0;
+      
+      //move
+      playerX--;
+
+      //set new player location
+      grid[playerY][playerX] = 9;
+    }
+  }
+
+  if (keyCode === UP_ARROW) {
+    if (grid[playerY-1][playerX] === 0) {
+      //reset old location to white
+      grid[playerY][playerX] = 0;
+      
+      //move
+      playerY--;
+
+      //set new player location
+      grid[playerY][playerX] = 9;
+    }
+  }
+
+  if (keyCode === DOWN_ARROW) {
+    if (grid[playerY+1][playerX] === 0) {
+      //reset old location to white
+      grid[playerY][playerX] = 0;
+      
+      //move
+      playerY++;
+
+      //set new player location
+      grid[playerY][playerX] = 9;
+    }
+  }
 }
 
 function mousePressed() {
@@ -108,6 +98,9 @@ function displayGrid(grid) {
       }
       else if (grid[y][x] === 1) {
         fill("black");
+      }
+      else if (grid[y][x] === 9) {
+        fill("red");
       }
       rect(x*cellWidth, y*cellHeight, cellWidth, cellHeight);
     }
--- moving/sketch.js
+++ moving/sketch.js
@@ -1,61 +1,128 @@
-// Randomized 2D Grid
-// Michael Gao
-// 10/26/2022
+// Randomized 2d Grid
+// Schellenberg
+// Oct 26, 2022
 
 const ROWS = 40;
-const COLMS = 40;
-let grid; 
+const COLS = 40;
+let grid;
 let cellWidth;
 let cellHeight;
+let playerX = 0;
+let playerY = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  cellWidth = width/COLMS;
-  cellHeight = height ROWS;
-  grid = createRandom2dArray(COLMS, ROWS); //colms and ROWS values
+  cellWidth = width/COLS;
+  cellHeight = height/ROWS;
+  grid = createRandom2dArray(COLS, ROWS);
+  //place player in grid
+  grid[playerY][playerX] = 9;
 }
 
 function draw() {
   background(220);
+  displayGrid(grid);
 }
 
-function mousePressed(){
+function keyPressed() {
+  if (keyCode === RIGHT_ARROW) {
+    if (grid[playerY][playerX+1] === 0) {
+      //reset old location to white
+      grid[playerY][playerX] = 0;
+      
+      //move
+      playerX++;
+
+      //set new player location
+      grid[playerY][playerX] = 9;
+    }
+  }
+
+  if (keyCode === LEFT_ARROW) {
+    if (grid[playerY][playerX-1] === 0) {
+      //reset old location to white
+      grid[playerY][playerX] = 0;
+      
+      //move
+      playerX--;
+
+      //set new player location
+      grid[playerY][playerX] = 9;
+    }
+  }
+
+  if (keyCode === UP_ARROW) {
+    if (grid[playerY-1][playerX] === 0) {
+      //reset old location to white
+      grid[playerY][playerX] = 0;
+      
+      //move
+      playerY--;
+
+      //set new player location
+      grid[playerY][playerX] = 9;
+    }
+  }
+
+  if (keyCode === DOWN_ARROW) {
+    if (grid[playerY+1][playerX] === 0) {
+      //reset old location to white
+      grid[playerY][playerX] = 0;
+      
+      //move
+      playerY++;
+
+      //set new player location
+      grid[playerY][playerX] = 9;
+    }
+  }
+}
+
+function mousePressed() {
   let xPos = Math.floor(mouseX/cellWidth);
   let yPos = Math.floor(mouseY/cellHeight);
-    if (grid[yPos])
+
+  if (grid[yPos][xPos] === 0) {
+    grid[yPos][xPos] = 1;
+  }
+  else if (grid[yPos][xPos] === 1) {
+    grid[yPos][xPos] = 0;
+  }
 }
 
 function displayGrid(grid) {
-  for (let y=0; y ROWS; y++) {
-    for (let x=0; x<COLMS; x++) {
+  for (let y=0; y<ROWS; y++) {
+    for (let x=0; x<COLS; x++) {
       if (grid[y][x] === 0) {
         fill("white");
       }
       else if (grid[y][x] === 1) {
         fill("black");
       }
+      else if (grid[y][x] === 9) {
+        fill("red");
+      }
       rect(x*cellWidth, y*cellHeight, cellWidth, cellHeight);
     }
   }
 }
 
-
-function create2dArray(COLMS, ROWS) {
+function create2dArray(COLS, ROWS) {
   let emptyArray = [];
-  for (let y = 0; y < ROWS; y++) {
+  for (let y=0; y<ROWS; y++) {
     emptyArray.push([]);
-    for (let x = 0; x < COLMS; x++) { //How many colms you want
+    for (let x=0; x<COLS; x++) {
       emptyArray[y].push(0);
     }
   }
   return emptyArray;
 }
 
-function createRandom2dArray(COLMS, ROWS) {
+function createRandom2dArray(COLS, ROWS) {
   let emptyArray = [];
-  for (let y=0; y ROWS; y++) {
+  for (let y=0; y<ROWS; y++) {
     emptyArray.push([]);
-    for (let x=0; x<COLMS; x++) {
+    for (let x=0; x<COLS; x++) {
       if (random(100) < 50) {
         emptyArray[y].push(0);
       }
