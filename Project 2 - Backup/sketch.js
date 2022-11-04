4befbdb1b3472670dc80a221c703f41ccbd98868
chineseperson0645 <114017596+chineseperson0645@users.noreply.github.com>
2022-11-04 07:46:40 -0600

    Update sketch.js

--- Project 2 - Backup/sketch.js
+++ Project 2 - Backup/sketch.js
@@ -1,6 +1,6 @@
-// Randomized 2d Grid
-// Schellenberg
-// Oct 26, 2022
+// X's and O's
+// Michael
+// 11/3/22
 
 const ROWS = 3;
 const COLS = 3;
@@ -20,30 +20,47 @@ function draw() {
   displayGrid(grid);
 }
 
-
 function mousePressed() {
   let xPos = Math.floor(mouseX/cellWidth);
   let yPos = Math.floor(mouseY/cellHeight);
+  let xPos2 = Math.floor(mouseX/cellWidth);
+  let yPos2 = Math.floor(mouseY/cellHeight);
 
+  //X
   if (grid[yPos][xPos] === 0) {
     grid[yPos][xPos] = 1;
   }
   else if (grid[yPos][xPos] === 1) {
     grid[yPos][xPos] = 0;
   }
+
+  //O
+  if (grid[yPos2][xPos2] === 2) {
+    grid[yPos2][xPos2] = 3;
+  }
+  else if (grid[yPos][xPos] === 3){
+    grid[yPos2][xPos2] = 2;
+  }
 }
 
 function displayGrid(grid) {
   for (let y=0; y<ROWS; y++) {
     for (let x=0; x<COLS; x++) {
+
+      //X
       if (grid[y][x] === 0) {
         fill("white");
       }
       else if (grid[y][x] === 1) {
         fill("black");
       }
-      else if (grid[y][x] === 9) {
+
+      //O
+      if (grid[y][x] === 2){
         fill("red");
+      }
+      else if (grid[y][x] === 3){
+        fill("blue");
       }
       rect(x*cellWidth, y*cellHeight, cellWidth, cellHeight);
     }
