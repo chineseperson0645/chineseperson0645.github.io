497f6278af2aec184bd844967a9418ad30449094
chineseperson0645 <114017596+chineseperson0645@users.noreply.github.com>
2022-11-07 09:08:11 -0600

    k

--- Project 2 - Backup/sketch.js
+++ Project 2 - Backup/sketch.js
@@ -17,68 +17,39 @@ let XImg;
 let widthOfCanvas = 800;
 let heightOfCanvas = 700; 
 
-// class cell1 {
-//   constructor (xOccupied, yOccupied);
-  
-//   cell1XOccupied: "no",
-//   cell1OOccupied: "no",
-//   cell2XOccupied: "no",
-//   cell2OOccupied: "no",
-//   cell3XOccupied: "no",
-//   cell3OOccupied: "no",
-//   cell4XOccupied: "no",
-//   cell4OOccupied: "no",
-//   cell5XOccupied: "no",
-//   cell5OOccupied: "no",
-//   cell6XOccupied: "no",
-//   cell6OOccupied: "no",
-//   cell7XOccupied: "no",
-//   cell7OOccupied: "no",
-//   cell8XOccupied: "no",
-//   cell8OOccupied: "no",
-//   cell9XOccupied: "no",
-//   cell9OOccupied: "no"
-// }
-
-const cell1 = {
-  XOccupied: "no",
-  OOccupied: "no",
-}
-const cell2 = {
-  cell2XOccupied: "no",
-  cell2OOccupied: "no"
-}
-const cell3 = {
-  cell3XOccupied: "no",
-  cell3OOccupied: "no"
-}
-const cell4 = {
-  cell4XOccupied: "no",
-  cell4OOccupied: "no"
-}
-const cell5 = {
-  cell5XOccupied: "no",
-  cell5OOccupied: "no"
-}
-const cell6 = {
-  cell6XOccupied: "no",
-  cell6OOccupied: "no"
-}
-const cell7 = {
-  cell7XOccupied: "no",
-  cell7OOccupied: "no"
-}
-
-const cell8 = {
-  cell8XOccupied: "no",
-  cell8OOccupied: "no"
-}
-const cell9 = {
-  cell9XOccupied: "no",
-  cell9OOccupied: "no"
-}
 
 
+function cellsChecker() {
+  let cells = {
+    cell1XOccupied: "no",
+    cell1OOccupied: "no",
+
+    cell2XOccupied: "no",
+    cell2OOccupied: "no",
+
+    cell3XOccupied: "no",
+    cell3OOccupied: "no",
+
+    cell4XOccupied: "no",
+    cell4OOccupied: "no",
+
+    cell5XOccupied: "no",
+    cell5OOccupied: "no",
+
+    cell6XOccupied: "no",
+    cell6OOccupied: "no",
+
+    cell7XOccupied: "no",
+    cell7OOccupied: "no",
+
+    cell8XOccupied: "no",
+    cell8OOccupied: "no",
+
+    cell9XOccupied: "no",
+    cell9OOccupied: "no"
+  };
+  grid.push(cells);
+}
 
 function preload() {
   OImg = loadImage('O.png');
@@ -89,8 +60,8 @@ function preload() {
 
 function setup() {
   createCanvas(widthOfCanvas, heightOfCanvas);
-  cellWidth = width/COLS;
-  cellHeight = height/ROWS;
+  cellWidth = width/COLS; //Overall Width divided by COLS value (# of COLS)
+  cellHeight = height/ROWS; //Overall Height divided by ROWS vlaue (# of ROWS)
   grid = create2dArray(COLS, ROWS);
 }
 
@@ -112,9 +83,9 @@ function mousePressed() {
     grid[yPos][xPos] = 1;
     oTurn = true;
 
-  if (grid[yPos][xPos] = 1){
-    cell1.XOccupied = "yes";
-      if (cell1.XOccupied === "yes"){
+  if (grid[yPos][xPos] === 1){
+    cell1XOccupied = "yes";
+      if (cell1XOccupied === "yes"){
         console.log("X.");
 
       }
@@ -129,15 +100,16 @@ function mousePressed() {
     grid[yPos][xPos] = 2;
     oTurn = false;
 
-    if (grid[yPos][xPos] = 2){
-      cell1.OOccupied = "yes";
-        if (cell1.OOccupied === "yes"){
+    if (grid[yPos][xPos] === 2){
+      cell1OOccupied = "yes";
+        if (cell1OOccupied === "yes"){
           console.log("O.");
+  
+        }
       }
+    if (grid[yPos][xPos] === 2 && grid[yPos][xPos+1] === 2 && grid[yPos][xPos+2] === 2){
+      console.log("O Wins!");
     }
-    // if (grid[yPos][xPos] === 0){
-
-    // }
   }
 }
 
@@ -177,27 +149,95 @@ function displayGrid(grid) {
   }
 }
 
-
-
-
-
 function create2dArray(COLS, ROWS) {
   let emptyArray = [];
   for (let y=0; y<ROWS; y++) {
-    emptyArray.push([]);
+    emptyArray.push([]); 
+    //Creates another array within the Array
+    //(by filling it with empty space)
+
     for (let x=0; x<COLS; x++) {
       emptyArray[y].push(0);
+    //Within the empty array (within an array)
+    //it pushes 0 values, mousePressed and displayGrid
+    //Function off of these zero values by checking and 
+    //Adding off of them according to their instructions
     }
   }
   return emptyArray;
+  //Pushes emptyArray as the grid, check above within setup function
+  //for "grid = create2dArray(COLS, ROWS);"
 }
 
+//^
+// 0, 0, 0
+// 0, 0, 0
+// 0, 0, 0
+
+//^
+//According to the ROWS and COLS 
+//Values
+
+
+
 //Notes//
+// class cell1 {
+//   constructor (xOccupied, yOccupied);
+  
+//   cell1XOccupied: "no",
+//   cell1OOccupied: "no",
+//   cell2XOccupied: "no",
+//   cell2OOccupied: "no",
+//   cell3XOccupied: "no",
+//   cell3OOccupied: "no",
+//   cell4XOccupied: "no",
+//   cell4OOccupied: "no",
+//   cell5XOccupied: "no",
+//   cell5OOccupied: "no",
+//   cell6XOccupied: "no",
+//   cell6OOccupied: "no",
+//   cell7XOccupied: "no",
+//   cell7OOccupied: "no",
+//   cell8XOccupied: "no",
+//   cell8OOccupied: "no",
+//   cell9XOccupied: "no",
+//   cell9OOccupied: "no"
+// }
 
-  // else if (grid[yPos][xPos] === 1) {
-  //   grid[yPos][xPos] = 2;
-  // }
+// const cell1 = {
+//   XOccupied: "no",
+//   OOccupied: "no",
+// }
+// const cell2 = {
+//   cell2XOccupied: "no",
+//   cell2OOccupied: "no"
+// }
+// const cell3 = {
+//   cell3XOccupied: "no",
+//   cell3OOccupied: "no"
+// }
+// const cell4 = {
+//   cell4XOccupied: "no",
+//   cell4OOccupied: "no"
+// }
+// const cell5 = {
+//   cell5XOccupied: "no",
+//   cell5OOccupied: "no"
+// }
+// const cell6 = {
+//   cell6XOccupied: "no",
+//   cell6OOccupied: "no"
+// }
+// const cell7 = {
+//   cell7XOccupied: "no",
+//   cell7OOccupied: "no"
+// }
 
-  // else if (grid[yPos][xPos] === 3){
-  //   grid[yPos2][xPos2] = 0;
-  // }
+// const cell8 = {
+//   cell8XOccupied: "no",
+//   cell8OOccupied: "no"
+// }
+// const cell9 = {
+//   cell9XOccupied: "no",
+//   cell9OOccupied: "no"
+// }
