3556d6b98bf34547f7a063a3e58dc2ecb7bbf246
chineseperson0645 <114017596+chineseperson0645@users.noreply.github.com>
2022-11-21 11:21:12 -0600

    BRAN

---
+++ OOP - Button/sketch.js
@@ -0,0 +1,23 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+class Button{
+  constructor(x, y){
+    this.bWidth = bWidth;
+    this.bHieght = bHieght;
+    this.
+  }
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
+
